8fc717e1f37e4b2b32853613f0b951d633e881aa
emmi-lili <emmi.a.rivero@gmail.com>
2026-01-27 23:44:33 -0400

    Adding one voter hihi

--- packages/hardhat/scripts/addVoter.ts
+++ packages/hardhat/scripts/addVoter.ts
@@ -1,19 +1,18 @@
 import { ethers, deployments } from "hardhat";
 
 async function main() {
-  const [signer] = await ethers.getSigners();
-  const signerAddress = await signer.getAddress();
-  console.log("Adding deployer address:", signerAddress);
+  const voterAddress = "0xDBC8813F9fEF6b24D14AbC9077D73f9668E47266";
+  console.log("Adding voter address:", voterAddress);
 
   const Voting = await deployments.get("Voting");
   const voting = await ethers.getContractAt("Voting", Voting.address);
 
-  const tx = await voting.addVoters([signerAddress], [true]);
+  const tx = await voting.addVoters([voterAddress], [true]);
   await tx.wait();
 
   console.log("✅ Added to allowlist!");
 
-  const voterData = await voting.getVoterData(signerAddress);
+  const voterData = await voting.getVoterData(voterAddress);
   console.log("Is voter:", voterData[0]);
 }
 
